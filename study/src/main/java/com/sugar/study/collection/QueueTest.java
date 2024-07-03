12845bee11ba7b4a3ef1d94412ff76e600dfaec7
sugar <470420476@qq.com>
2024-07-03 17:21:32 +0800

    Java集合

---
+++ study/src/main/java/com/sugar/study/collection/QueueTest.java
@@ -0,0 +1,11 @@
+package com.sugar.study.collection;
+
+import java.util.HashSet;
+import java.util.Queue;
+
+public class QueueTest {
+    public static void main(String[] args) {
+//        Queue
+//        HashSet
+    }
+}
