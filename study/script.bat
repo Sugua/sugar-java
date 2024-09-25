dc256322468499dd677695d65d6087c2ae73000a
sugar <470420476@qq.com>
2024-09-25 10:09:59 +0800

    这是修改日志的神器

--- study/script.bat
+++ study/script.bat
@@ -1,4 +1,4 @@
-@echo off
+#!/bin/sh
 git filter-branch --env-filter '
 OLD_EMAIL="chenxg3@asiainfo.com"
 CORRECT_NAME="sugar"
@@ -12,13 +12,8 @@ if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
 then
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-
-
-
-
-
-
-
-
 fi
 ' --tag-name-filter cat -- --branches --tags
+
+
+git push --force --tags origin 'refs/heads/*'
