01b35e3e997b99c54494c8dba747b685b6dcd9af
sugar <470420476@qq.com>
2024-11-01 14:47:25 +0800

    查找替换

--- study/script.bat
+++ study/script.bat
@@ -1,8 +1,8 @@
 #!/bin/sh
 git filter-branch --env-filter '
-OLD_EMAIL="chenxg3@asiainfo.com"
-CORRECT_NAME="sugar"
-CORRECT_EMAIL="470420476@qq.com"
+OLD_EMAIL="470420476@qq.com"
+CORRECT_NAME="chenxg3"
+CORRECT_EMAIL="chenxg3@asiainfo.com"
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
@@ -14,6 +14,7 @@ then
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
 ' --tag-name-filter cat -- --branches --tags
+
 
 
 git push --force --tags origin 'refs/heads/*'
