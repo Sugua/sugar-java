e2b82a008d55872823efbbad9bd5e524c55651b9
sugar <470420476@qq.com>
2024-12-23 20:28:27 +0800

    selector sort

---
+++ study/src/main/java/sort/SelectorSort.java
@@ -0,0 +1,33 @@
+package sort;
+
+public class SelectorSort {
+    public static void main(String[] args) {
+        int[] array = {10,1, 4, 3, 2, 5, 0};
+
+        BubbleSort.printArray(array);
+        sort(array);
+
+        BubbleSort.printArray(array);
+    }
+    public static void sort(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int k = 0;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (min > array[j]) {
+                    min = array[j];
+                    k = j;
+                }
+
+            }
+            if (k != 0) {
+                array[k] = array[i];
+                array[i] = min;
+            }
+
+        }
+
+    }
+}
