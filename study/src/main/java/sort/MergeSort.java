634071358a25f767ec33c4672e5773c278f7b1c7
sugar <470420476@qq.com>
2025-01-02 11:50:07 +0800

    selector sort

---
+++ study/src/main/java/sort/MergeSort.java
@@ -0,0 +1,10 @@
+package sort;
+
+public class MergeSort {
+    public int[] merge (int[] left,int[] right){
+
+        int[] result=new int[left.length+right.length];
+
+        return result;
+    }
+}
--- study/src/main/java/sort/SelectorSort.java
+++ study/src/main/java/sort/SelectorSort.java
@@ -1,12 +1,17 @@
 package sort;
 
+/**
+ * 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置。
+ *
+ * 再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+ */
 public class SelectorSort {
     public static void main(String[] args) {
 //        int[] array = {10,1, 4, 3, 2, 5, 0};
 //
 //        BubbleSort.printArray(array);
 //        sort(array);
-//
+//想·
 //        BubbleSort.printArray(array);
 
         double t=1;
